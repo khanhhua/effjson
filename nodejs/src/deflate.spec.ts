dc7d253a3bb7ba35020570cc35dcfe3f722e18de
Khanh Hua <giakhanh2487@gmail.com>
2025-08-23 09:36:11 +0200

    deflate with array flag

--- nodejs/src/deflate.spec.ts
+++ nodejs/src/deflate.spec.ts
@@ -15,8 +15,20 @@ describe('deflate non-nested single object', () => {
   it('deflate string:string map', () => {
     const actual = deflate({ name: 'Tom' });
 
-    assert.deepStrictEqual(actual.schema.root, [{ name: 'name', type: 'string' }]);
+    assert.deepStrictEqual(actual.schema.root, [{ name: 'name', type: 'string', flag: 0 }]);
     assert.deepStrictEqual(actual.data, ['Tom']);
+  });
+
+  it('deflate string:[string array of 1 element] map', () => {
+    const actual = deflate({ names: ['Tom'] });
+    assert.deepStrictEqual(actual.schema.root, [{ name: 'names', type: 'string', flag: 1 }]);
+    assert.deepStrictEqual(actual.data, [1, 'Tom'])
+  });
+
+  it('deflate string:[string array of 2 elements] map', () => {
+    const actual = deflate({ names: ['Tom', 'Jerry'] });
+    assert.deepStrictEqual(actual.schema.root, [{ name: 'names', type: 'string', flag: 1 }]);
+    assert.deepStrictEqual(actual.data, [2, 'Tom', 'Jerry'])
   });
 });
 
@@ -24,14 +36,28 @@ describe('deflate non-nested homogenous object array', () => {
   it('deflate string:string map', () => {
     const actual = deflate([{ name: 'Tom' }]);
 
-    assert.deepStrictEqual(actual.schema.root, [{ name: 'name', type: 'string' }]);
+    assert.deepStrictEqual(actual.schema.root, [{ name: 'name', type: 'string', flag: 0 }]);
     assert.deepStrictEqual(actual.data, ['Tom']);
+  });
+
+  it('deflate string:[string array of 1 element] map', () => {
+    const actual = deflate([{ names: ['Tom'] }]);
+
+    assert.deepStrictEqual(actual.schema.root, [{ name: 'names', type: 'string', flag: 1 }]);
+    assert.deepStrictEqual(actual.data, [1, 'Tom']);
+  });
+
+  it('deflate string:[string array of 2 elements] map', () => {
+    const actual = deflate([{ names: ['Tom', 'Jerry'] }]);
+
+    assert.deepStrictEqual(actual.schema.root, [{ name: 'names', type: 'string', flag: 1 }]);
+    assert.deepStrictEqual(actual.data, [2, 'Tom', 'Jerry']);
   });
 
   it('deflate string:string maps', () => {
     const actual = deflate([{ name: 'Tom' }, { name: 'Jerry' }]);
 
-    assert.deepStrictEqual(actual.schema.root, [{ name: 'name', type: 'string' }]);
+    assert.deepStrictEqual(actual.schema.root, [{ name: 'name', type: 'string', flag: 0 }]);
     assert.deepStrictEqual(actual.data, ['Tom', 'Jerry']);
   });
 
@@ -39,10 +65,23 @@ describe('deflate non-nested homogenous object array', () => {
     const actual = deflate([{ name: 'Tom', color: 'blue' }, { name: 'Jerry', color: 'orange' }]);
 
     assert.deepStrictEqual(actual.schema.root, [
-      { name: 'name', type: 'string' },
-      { name: 'color', type: 'string' },
+      { name: 'name', type: 'string', flag: 0 },
+      { name: 'color', type: 'string', flag: 0 },
     ]);
     assert.deepStrictEqual(actual.data, ['Tom', 'blue', 'Jerry', 'orange']);
+  });
+
+  it('deflate multiple keyed string:{string, [string array]} maps', () => {
+    const actual = deflate([
+      { name: 'Tom', colors: ['blue', 'white'] },
+      { name: 'Jerry', colors: ['orange', 'black'] }
+    ]);
+
+    assert.deepStrictEqual(actual.schema.root, [
+      { name: 'name', type: 'string', flag: 0 },
+      { name: 'colors', type: 'string', flag: 1 },
+    ]);
+    assert.deepStrictEqual(actual.data, ['Tom', 2, 'blue', 'white', 'Jerry', 2, 'orange', 'black']);
   });
 });
 
@@ -52,11 +91,13 @@ describe('deflate 1st degree nested homogenous object', () => {
 
     assert.deepStrictEqual(actual.schema.root, [{
       name: 'student',
-      type: '@student'
+      type: '@student',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.schema['@student'], [{
       name: 'name',
-      type: 'string'
+      type: 'string',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.data, ['Tom']);
   });
@@ -66,15 +107,18 @@ describe('deflate 1st degree nested homogenous object', () => {
 
     assert.deepStrictEqual(actual.schema.root, [{
       name: 'student',
-      type: '@student'
+      type: '@student',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.schema['@student'], [{
       name: 'name',
-      type: 'string'
+      type: 'string',
+      flag: 0
     },
     {
       name: 'color',
-      type: 'string'
+      type: 'string',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.data, ['Tom', 'blue']);
   });
@@ -86,15 +130,18 @@ describe('deflate 2nd degree nested homogenous object', () => {
 
     assert.deepStrictEqual(actual.schema.root, [{
       name: 'room',
-      type: '@room'
+      type: '@room',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.schema['@room'], [{
       name: 'student',
-      type: '@student'
+      type: '@student',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.schema['@student'], [{
       name: 'name',
-      type: 'string'
+      type: 'string',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.data, ['Tom']);
   });
@@ -104,21 +151,91 @@ describe('deflate 2nd degree nested homogenous object', () => {
 
     assert.deepStrictEqual(actual.schema.root, [{
       name: 'room',
-      type: '@room'
+      type: '@room',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.schema['@room'], [{
       name: 'student',
-      type: '@student'
+      type: '@student',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.schema['@student'], [{
       name: 'name',
-      type: 'string'
+      type: 'string',
+      flag: 0
     },
     {
       name: 'color',
-      type: 'string'
+      type: 'string',
+      flag: 0
     }]);
     assert.deepStrictEqual(actual.data, ['Tom', 'blue']);
+
+    it('deflate string: [string: array of [string:string; string:string]] map', () => {
+      const actual = deflate({
+        room: {
+          students: [
+            { name: 'Tom', color: 'blue' },
+            { name: 'Jerry', color: 'orange' }
+          ]
+        }
+      });
+
+      assert.deepStrictEqual(actual.schema.root, [{
+        name: 'room',
+        type: '@room',
+        flag: 0
+      }]);
+      assert.deepStrictEqual(actual.schema['@room'], [{
+        name: 'students',
+        type: '@students',
+        flag: 1
+      }]);
+      assert.deepStrictEqual(actual.schema['@students'], [{
+        name: 'name',
+        type: 'string',
+        flag: 0
+      },
+      {
+        name: 'color',
+        type: 'string',
+        flag: 0
+      }]);
+      assert.deepStrictEqual(actual.data, [2, 'Tom', 'blue', 'Jerry', 'orange']);
+    });
+  });
+
+  it('deflate string: [string: array of [string:string; string: string array]] map', () => {
+    const actual = deflate({
+      room: {
+        students: [
+          { name: 'Tom', colors: ['blue', 'white'] },
+          { name: 'Jerry', colors: ['orange', 'black'] }
+        ]
+      }
+    });
+
+    assert.deepStrictEqual(actual.schema.root, [{
+      name: 'room',
+      type: '@room',
+      flag: 0
+    }]);
+    assert.deepStrictEqual(actual.schema['@room'], [{
+      name: 'students',
+      type: '@students',
+      flag: 1
+    }]);
+    assert.deepStrictEqual(actual.schema['@students'], [{
+      name: 'name',
+      type: 'string',
+      flag: 0
+    },
+    {
+      name: 'colors',
+      type: 'string',
+      flag: 1
+    }]);
+    assert.deepStrictEqual(actual.data, [2, 'Tom', 2, 'blue', 'white', 'Jerry', 2, 'orange', 'black']);
   });
 });
 
--- nodejs/src/types.ts
+++ nodejs/src/types.ts
@@ -6,9 +6,14 @@ export enum PrimitiveType {
 export interface Meta {
   name: string;
   type: PrimitiveType | string;
+  flag: Flag;
 }
 
 export type Datum = string | number | null;
+
+export enum Flag {
+  Single, Multiple
+}
 
 export interface Schema {
   root: Meta[];
